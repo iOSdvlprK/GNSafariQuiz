f78a780f7d01d662fe1b245fee0641c32136a320
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-06-11 23:08:41 +0900

    Initial Commit

---
+++ GNSafariQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GNSafariQuiz
+//
+//  Created by joe on 6/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
