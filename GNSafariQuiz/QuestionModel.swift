6e53bb5f2a3c543508bc91e9381179c0e7f69837
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-06-12 22:59:37 +0900

    setup quiz UI and QuestionModel
    - add buffalo, giraffe, and lion images to Assets
    - create QuestionModel struct for quiz data
    - build quiz layout in Main.storyboard with Auto Layout
    - connect IBOutlets and IBActions in ViewController

---
+++ GNSafariQuiz/QuestionModel.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionModel.swift
+//  GNSafariQuiz
+//
+//  Created by joe on 6/11/26.
+//
+
+import UIKit
+
+struct QuestionModel {
+    let image: UIImage
+    let correctAnswer: Int
+    let answer1: String
+    let answer2: String
+    let answer3: String
+}
--- GNSafariQuiz/ViewController.swift
+++ GNSafariQuiz/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var questionImageView: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var answer1Button: UIButton!
+    @IBOutlet weak var answer2Button: UIButton!
+    @IBOutlet weak var answer3Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func answer1ButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func answer2ButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func answer3ButtonTapped(_ sender: Any) {
+    }
 }
 
