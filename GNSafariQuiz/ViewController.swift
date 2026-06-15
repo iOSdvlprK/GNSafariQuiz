33433c305e1fc3c18e864501fa8544946190b2da
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-06-15 22:31:28 +0900

    implement basic quiz logic and score tracking
    - initialize UI with the first question in viewDidLoad
    - add score variable and update scoreLabel on correct answers
    - implement answer validation logic for all button taps

--- GNSafariQuiz/ViewController.swift
+++ GNSafariQuiz/ViewController.swift
@@ -18,19 +18,49 @@ class ViewController: UIViewController {
         QuestionModel(image: UIImage(resource: .giraffe), correctAnswer: 1, answer1: "Giraffe", answer2: "Crocodile", answer3: "Elephant"),
         QuestionModel(image: UIImage(resource: .buffalo), correctAnswer: 3, answer1: "Hippo", answer2: "Lion", answer3: "Buffalo")
     ]
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let currentQuestion = questions[0]
+        questionImageView.image = currentQuestion.image
+        answer1Button.setTitle(currentQuestion.answer1, for: .normal)
+        answer2Button.setTitle(currentQuestion.answer2, for: .normal)
+        answer3Button.setTitle(currentQuestion.answer3, for: .normal)
     }
 
     @IBAction func answer1ButtonTapped(_ sender: Any) {
+        let currentQuestion = questions[0]
+        if currentQuestion.correctAnswer == 1 {
+            print("Answer is correct!")
+            score += 1
+            scoreLabel.text = "Score \(score)"
+        } else {
+            print("Answer incorrect.")
+        }
     }
     
     @IBAction func answer2ButtonTapped(_ sender: Any) {
+        let currentQuestion = questions[0]
+        if currentQuestion.correctAnswer == 2 {
+            print("Answer is correct!")
+            score += 1
+            scoreLabel.text = "Score \(score)"
+        } else {
+            print("Answer incorrect.")
+        }
     }
     
     @IBAction func answer3ButtonTapped(_ sender: Any) {
+        let currentQuestion = questions[0]
+        if currentQuestion.correctAnswer == 3 {
+            print("Answer is correct!")
+            score += 1
+            scoreLabel.text = "Score \(score)"
+        } else {
+            print("Answer incorrect.")
+        }
     }
 }
 
